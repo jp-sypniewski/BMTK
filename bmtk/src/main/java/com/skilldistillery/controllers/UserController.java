da16deb76424c8ecd11a7bc753500c463fab0246
sgmerwin <stevegmerwin@gmail.com>
2020-02-22 14:22:52 -0700

    nothing

--- bmtk/src/main/java/com/skilldistillery/controllers/UserController.java
+++ bmtk/src/main/java/com/skilldistillery/controllers/UserController.java
@@ -1,12 +1,18 @@
 package com.skilldistillery.controllers;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
-@CrossOrigin({"*", "http://localhost:4201"})
+@CrossOrigin({"*", "http://localhost:4200"})
 public class UserController {
+	
+	@GetMapping("ping")
+	public String pingPong() {
+		return "pong";
+	}
 
 }
