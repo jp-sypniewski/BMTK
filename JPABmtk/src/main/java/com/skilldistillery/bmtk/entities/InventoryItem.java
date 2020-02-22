334cd7e84921c5e55abae60f5821e86d06ded6ec
Travis Roberts <travis.c.roberts35@gmail.com>
2020-02-22 14:20:12 -0700

    added InventoryItem, and tested with JUnit, also ran gradle tests. all passed.

---
+++ JPABmtk/src/main/java/com/skilldistillery/bmtk/entities/InventoryItem.java
@@ -0,0 +1,103 @@
+package com.skilldistillery.bmtk.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="inventory_item")
+public class InventoryItem {
+	
+	//Fields
+
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private int id;
+	
+		private String name;
+		private Integer quantity;
+		private Double value;
+		
+		
+		public InventoryItem() {
+			super();
+		}
+		public InventoryItem(int id, String name, Integer quantity, Double value) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.quantity = quantity;
+			this.value = value;
+		}
+		@Override
+		public String toString() {
+			return "InventoryItem [id=" + id + ", name=" + name + ", quantity=" + quantity + ", value=" + value + "]";
+		}
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public Integer getQuantity() {
+			return quantity;
+		}
+		public void setQuantity(Integer quantity) {
+			this.quantity = quantity;
+		}
+		public Double getValue() {
+			return value;
+		}
+		public void setValue(Double value) {
+			this.value = value;
+		}
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + id;
+			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			result = prime * result + ((quantity == null) ? 0 : quantity.hashCode());
+			result = prime * result + ((value == null) ? 0 : value.hashCode());
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			InventoryItem other = (InventoryItem) obj;
+			if (id != other.id)
+				return false;
+			if (name == null) {
+				if (other.name != null)
+					return false;
+			} else if (!name.equals(other.name))
+				return false;
+			if (quantity == null) {
+				if (other.quantity != null)
+					return false;
+			} else if (!quantity.equals(other.quantity))
+				return false;
+			if (value == null) {
+				if (other.value != null)
+					return false;
+			} else if (!value.equals(other.value))
+				return false;
+			return true;
+		}
+		
+		
+	
+}
--- JPABmtk/src/test/java/com/skilldistillery/bmtk/InventoryItemTests.java
+++ JPABmtk/src/test/java/com/skilldistillery/bmtk/InventoryItemTests.java
@@ -1,7 +1,6 @@
 package com.skilldistillery.bmtk;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -13,13 +12,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.skilldistillery.bmtk.entities.Customer;
-import com.skilldistillery.bmtk.entities.User;
+import com.skilldistillery.bmtk.entities.InventoryItem;
+import com.skilldistillery.bmtk.entities.Task;
 
-class CustomerTests {
+class InventoryItemTests {
 	private EntityManager em;
 	private static EntityManagerFactory emf;
-	private Customer cust;
+	private InventoryItem item;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -34,20 +33,19 @@ class CustomerTests {
 	@BeforeEach
 	void setUp() throws Exception {
 		em = emf.createEntityManager();
-		cust = em.find(Customer.class, 1);
+		item = em.find(InventoryItem.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 		em.close();
-		cust = null;
+		item = null;
 	}
 
 	@Test
 	void test() {
-		assertNotNull(cust);
-		assertEquals("paypal", cust.getPaymentMethod());
-		assertEquals(4, cust.getuDetail().getId());
+		assertNotNull(item);
+		assertEquals("test item", item.getName());
 	}
 
 }
