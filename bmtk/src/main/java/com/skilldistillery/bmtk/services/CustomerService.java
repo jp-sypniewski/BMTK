e9e747fc66f4e0e20b7b2b4421091962b01882d9
jp-sypniewski <jpsypniewski@icloud.com>
2020-02-25 16:24:37 -0700

    updated company, customer, project, task to include active modifier in put service methods

---
+++ bmtk/src/main/java/com/skilldistillery/bmtk/services/CustomerService.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.bmtk.services;
+
+import com.skilldistillery.bmtk.entities.Customer;
+
+public interface CustomerService {
+
+	Customer updateCustomer(Integer cid, Customer customer);
+
+}
