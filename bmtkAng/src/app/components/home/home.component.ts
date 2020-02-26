c7d2188ea037ec6285f848b163ac1956e2727363
jp-sypniewski <jpsypniewski@icloud.com>
2020-02-26 16:23:18 -0700

    move new company form to home

--- bmtkAng/src/app/components/home/home.component.ts
+++ bmtkAng/src/app/components/home/home.component.ts
@@ -1,3 +1,5 @@
+import { CompanyService } from './../../services/company/company.service';
+import { Company } from './../../models/company';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -7,12 +9,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  company: Company = new Company();
+  companyCreated: string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private compSvc: CompanyService) { }
 
   ngOnInit(): void {
   }
 
+  createCompany(){
+    this.companyCreated = "";
 
 
+    this.compSvc.createCompany(this.company).subscribe(
+      data => {
+        console.log('RegisterComponent.register(): company registered.');
+        this.company = new Company();
+        //this.router.navigate(['companynew']);
+      },
+      err => {
+        console.error('RegisterComponent.register(): error registering.');
+        this.companyCreated = "Error Creating Company";
+      }
+    );
+
+}
 }
