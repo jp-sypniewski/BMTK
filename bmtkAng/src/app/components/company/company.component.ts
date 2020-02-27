8df0432f6fd8c5e9eda3c10b1a6c3837570c392b
jp-sypniewski <jpsypniewski@icloud.com>
2020-02-27 10:23:54 -0700

    adding html logic within company and home components

--- bmtkAng/src/app/components/company/company.component.ts
+++ bmtkAng/src/app/components/company/company.component.ts
@@ -11,15 +11,48 @@ export class CompanyComponent implements OnInit {
 
   projectsForCompany: Project[] = [];
   selectedProject: Project;
+  newTask: Task;
+  editingATask: Boolean;
 
 
   empTasksToDo: Task[] = [];
 
   projectsRequested: Project[] = [];
+  newProject: Project;
 
   constructor() { }
 
   ngOnInit(): void {
+
+
+  }
+
+  showProjectDetails(project){
+    this.selectedProject = project;
+  }
+
+  showCreateTask(){
+    this.newTask = new Task();
+    this.editingATask = false;
+  }
+
+  saveNewTask(){
+  }
+
+  showEditTask(task){
+    this.newTask = task;
+    this.editingATask = true;
+  }
+
+  saveEditTask(){
+  }
+
+  showRequestNewProject(){
+    this.newProject = new Project();
+  }
+
+  saveRequestNewProject(){
+
   }
 
 }
--- bmtkAng/src/app/components/home/home.component.ts
+++ bmtkAng/src/app/components/home/home.component.ts
@@ -23,8 +23,12 @@ export class HomeComponent implements OnInit {
     this.reload();
   }
 
-  createCompany(){
+  showCreateCompany(){
     this.newCompany = new Company();
+
+  }
+
+  saveCreateCompany(){
     this.companyCreated = "";
 
 
