9127f2abc1649a8c58b9f0991b45fb23a867d0b6
jp-sypniewski <jpsypniewski@icloud.com>
2020-03-01 15:48:01 -0700

    updating employee model, add employee array to company and task

--- bmtkAng/src/app/models/employee.ts
+++ bmtkAng/src/app/models/employee.ts
@@ -1,8 +1,21 @@
+import { Task } from 'src/app/models/task';
+import { Company } from 'src/app/models/company';
+import { UserDetail } from './userDetail';
+
 export class Employee {
 
   id: number;
+  userDetail: UserDetail;
+  company: Company;
+  active: Boolean;
+  tasks: Task[];
 
-  constructor(id?: number){
+  constructor(id?: number, userDetail?: UserDetail, company?: Company,
+    active?: Boolean, tasks?: Task[]){
     this.id = id;
+    this.userDetail = userDetail;
+    this.company = company;
+    this.active = active;
+    this.tasks = tasks;
   }
 }
